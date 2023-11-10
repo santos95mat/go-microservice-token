237e1b6623729e83fc772188b1499416f99d42fe
Matheus Rodrigues Santos <santos95.mat@gmail.com>
2023-11-10 10:20:44 -0300

    feat: create load enviroment variables

---
+++ init/load_env_variables.go
@@ -0,0 +1,14 @@
+package init
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
