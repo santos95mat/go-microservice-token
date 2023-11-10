61a4dd8800d6b6e1016bea6f536fa7ac128b2332
Matheus Rodrigues Santos <santos95.mat@gmail.com>
2023-11-10 14:43:36 -0300

    feat: create token routes

--- internal/app/token_routes.go
+++ internal/app/token_routes.go
@@ -2,14 +2,11 @@ package app
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/santos95mat/go-microservice-token/internal/handle"
 )
 
 func addTokenRoutes(token fiber.Router) {
-	token.Post("/Token", func(c *fiber.Ctx) error {
-		return c.SendString("Create a token")
-	})
+	token.Post("/", handle.TokenHandler.Create)
 
-	token.Get("/Token", func(c *fiber.Ctx) error {
-		return c.SendString("Get a token")
-	})
+	token.Get("/", handle.TokenHandler.GetOne)
 }
