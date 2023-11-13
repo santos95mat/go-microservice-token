fbf2e55e2b58f22ae48f0861c12dad1239919510
Matheus Rodrigues Santos <santos95.mat@gmail.com>
2023-11-13 10:54:54 -0300

    feat: implemented util unit test

---
+++ internal/util/token_generator_test.go
@@ -0,0 +1,22 @@
+package util_test
+
+import (
+	"testing"
+
+	"github.com/santos95mat/go-microservice-token/internal/util"
+)
+
+func testGenerate(t *testing.T) {
+	tokenDto := util.RandonToken{
+		LowCaseQuantity:     1,
+		UpCaseQuantity:      1,
+		NumbersQuantity:     1,
+		SpecialCharQuantity: 1,
+	}
+
+	randonToken := tokenDto.Generate()
+
+	if len(randonToken) != 4 {
+		t.Error("O token recebido é diferente do esperado")
+	}
+}
